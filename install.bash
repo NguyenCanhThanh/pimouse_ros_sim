26a5b05edddf2096cbca7a40585b8e34ad61ce3a
Nguyễn Cảnh Thanh <48113236+NguyenCanhThanh@users.noreply.github.com>
2022-03-04 18:58:46 +0700

    Add files via upload

---
+++ install.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+UBUNTU_VER=$(lsb_release -sc)
+ROS_VER=melodic
+
+sudo apt-get install -y ros-${ROS_VER}-desktop-full
+[ "which mktemp" = "" ] && sudo apt-get install -y mktemp
+
+source ~/catkin_ws/devel/setup.bash || { echo "catkin_ws is not working correctly"; exit 1; }
+
+roscd && cd ..
+rosdep install -r -y --ignore-src --from-paths src
+roscd && cd .. && source ~/catkin_ws/devel/setup.bash && catkin_make
+
+source ~/catkin_ws/devel/setup.bash
+
+mkdir -p ~/.gazebo/models && cd ~/.gazebo/models
+if [ ! -e "ground_plane" ]; then
+	cd /tmp && \
+	TMPDIR=$(mktemp -d tmp.XXXXXXXXXX) && \
+	cd $TMPDIR && \
+	wget -l1 -np -nc -r "http://models.gazebosim.org/sun" --accept=gz && \
+	wget -l1 -np -nc -r "http://models.gazebosim.org/ground_plane" --accept=gz && \
+	wget -l1 -np -nc -r "http://models.gazebosim.org/gas_station" --accept=gz && \
+	cd "models.gazebosim.org" && \
+	for i in *; do tar -zvxf "$i/model.tar.gz"; done && \
+	cp -vfR * ~/.gazebo/models/
+fi
